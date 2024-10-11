8c77a8fc7b31abf1099914f78db7e4ab8f982640
PetrVrba03 <96355386+Mrprogramer885@users.noreply.github.com>
2024-10-11 12:11:17 +0200

    Update run.sh
    Updated info message.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 echo ---starting Xtool Terminal---
-echo this program was tested to work on termux and PC linux distros.
+echo this program was tested to work on termux and PC linux distros. Windows is not fully supported yet, so if using Xtool on Windows, expect it to be unstable.
 python3 modules/start.py
